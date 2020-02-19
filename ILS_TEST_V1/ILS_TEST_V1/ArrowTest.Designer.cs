9dade6df6d32ae88ffa9f583da7c2839e0fa6b92
top5559 <top5559@naver.com>
2020-02-19 12:20:49 +0900

    아이콘 생성

--- ILS_TEST_V1/ILS_TEST_V1/ArrowTest.Designer.cs
+++ ILS_TEST_V1/ILS_TEST_V1/ArrowTest.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArrowTest));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -143,6 +144,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ArrowTest";
             this.Text = "Arrow 검증";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- ILS_TEST_V1/ILS_TEST_V1/FileTest.Designer.cs
+++ ILS_TEST_V1/ILS_TEST_V1/FileTest.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileTest));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -89,6 +90,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FileTest";
             this.Text = "파일 검증";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
