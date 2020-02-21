8e087783bb41c82f6c19dc5b08bef162b163bbce
top5559 <top5559@naver.com>
2020-02-21 11:05:18 +0900

    1. 멀티 검증안의 파일 선택 버튼안에 코드들 메서드화 작업 2. 찾기 이벤트 발생시 그리드 행,열 초기화 구문 추가 3. 멀티 검증 gridveiw 안의 Columns 에 Index,Filename 등 데이터 추가 4. 멀티 검증 gridveiw 안의 Row에 INDEX값 넣기 5. 멀티 검증 각 열의 데이터에 맞게 자동으로 사이즈 조절 기능 추가

--- ILS_TEST_V1/ILS_TEST_V1/View/FileTest.cs
+++ ILS_TEST_V1/ILS_TEST_V1/View/FileTest.cs
@@ -22,26 +22,64 @@ namespace ILS_TEST_V1
 
         private void btnFolderSelect_Click(object sender, EventArgs e)
         {
+            //2020.02.21 메서드화 작업 코드수정 (박찬규)
+            FileSearch();
+        }
+
+        private void FileSearch()
+        {
             var fbd = new FolderBrowserDialog();
             var result = fbd.ShowDialog();
 
             if (result != DialogResult.OK)
+            {
                 return;
-            txtFolderPath.Text = fbd.SelectedPath; // 선택한 폴더명
+            }
 
+            // 선택한 폴더명 (최정웅)
+            txtFolderPath.Text = fbd.SelectedPath;
+
+            // 그리드 행, 열 초기화 (최정웅)
+            gridVerify.Columns.Clear();
+            gridVerify.Rows.Clear();
 
             var selectPath = txtFolderPath.Text;
             var files = Directory.GetFiles(selectPath, "*.psd", SearchOption.AllDirectories);
 
+            //DataGridView 열에 데이터 넣기(최정웅 박찬규)
             #region PSD FILE LIST
-            gridVerify.Columns.Add("FileName", "FileName");
+            gridVerify.ColumnCount = 6;
+            
+            gridVerify.Columns[0].Name = "Index";
+            gridVerify.Columns[1].Name = "FileName";
+            gridVerify.Columns[2].Name = "ILS_Type";
+            gridVerify.Columns[3].Name = "TotalCount";
+            gridVerify.Columns[4].Name = "Success";
+            gridVerify.Columns[5].Name = "Fail";
+
+            //gridVerify.Columns.Add("FileName", "FileName"); 
             #endregion
+           
+
+
+
+            //DataGridView 행에 값 넣는 곳 (최정웅 , 박찬규)
+            //순번 표기를 위한 Index 변수 추가(박찬규)
+            #region PSD FILE Data
+            int Index = 0;
+
             foreach (var file in files)
             {
+                ++Index;
                 var doc = PsdDocument.Create(file);
-                gridVerify.Rows.Add(file);
-            }
 
+                gridVerify.Rows.Add(Index, file);
+            }
+            #endregion
+
+            //각 열의 데이터에 맞게 자동으로 사이즈 조절 기능 추가 (박찬규)
+            gridVerify.AutoResizeColumns();
         }
+      
     }
 }
